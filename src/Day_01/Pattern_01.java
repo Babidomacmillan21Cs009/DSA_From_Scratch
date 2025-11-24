4fe11c65134c4b5cb2022f2677dfcc571dfdf6b5
Babidomacmillan21Cs009 <babidomacmillan@gmail.com>
2025-11-24 13:04:52 +0530

    done pattern_01

--- src/Day_01/Pattern_01.java
+++ src/Day_01/Pattern_01.java
@@ -1,4 +1,12 @@
 package Day_01;
 
 public class Pattern_01 {
+    public static void main(String[] args){
+        for (int i =0; i < 4; i++){
+            for (int j =0; j < 4; j++){
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+    }
 }
