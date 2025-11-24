198b842747d486acccd5317b2189d8041753d3b0
Babidomacmillan21Cs009 <babidomacmillan@gmail.com>
2025-11-24 14:37:56 +0530

    modified pattern_01

--- src/Day_01/Pattern_01.java
+++ src/Day_01/Pattern_01.java
@@ -1,12 +1,18 @@
 package Day_01;
 
 public class Pattern_01 {
-    public static void main(String[] args){
-        for (int i =0; i < 4; i++){
-            for (int j =0; j < 4; j++){
+
+    public static void print(int n){
+        for (int i = 0; i < n; i++){
+            for (int j = 0; j < n; j++){
                 System.out.print("*");
             }
             System.out.println(" ");
         }
+    }
+
+    public static void main(String[] args){
+        int n = 7;
+        print(n);
     }
 }
