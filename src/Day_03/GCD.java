cce477a9f71d3045c604947055214f120e62915b
Babidomacmillan21Cs009 <babidomacmillan@gmail.com>
2025-11-26 13:54:39 +0530

    some changes

--- src/Day_03/GCD.java
+++ src/Day_03/GCD.java
@@ -1,5 +1,12 @@
 package Day_03;
 
+//        The Greatest Common Divisor (GCD) of two integers is the largest positive integer that divides both of the integers.
+//            Input: n1 = 4, n2 = 6
+//            Output: 2
+//            Explanation: Divisors of n1 = 1, 2, 4, Divisors of n2 = 1, 2, 3, 6
+//            Greatest Common divisor = 2.
+
+
 public class GCD {
 
     public static int gcd(int a, int b){
