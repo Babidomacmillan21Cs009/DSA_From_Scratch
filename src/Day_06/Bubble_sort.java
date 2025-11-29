6c968b453f23c959c72339c140b4d38f896970c9
Babidomacmillan21Cs009 <babidomacmillan@gmail.com>
2025-11-29 23:07:53 +0530

    Learned bubble sort program

--- src/Day_06/Bubble_sort.java
+++ src/Day_06/Bubble_sort.java
@@ -5,7 +5,7 @@ public class Bubble_sort {
     public static void bubble_sort(int[] arr){
         int n = arr.length;
         for (int i = n - 1; i >= 1; i--){
-            for (int j = 0; j <= i - 1; j++){
+            for (int j = 0; j <= i - 1; j++){        // j goes to only before i because we are checking arr[j] > arr[j+1]
                 if (arr[j] > arr[j + 1]){
                     int temp = arr[j];
                     arr[j] = arr[j + 1];
