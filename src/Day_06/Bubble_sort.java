d5ff976b5be51f67c9e1a1e96cea47af05484bf7
Babidomacmillan21Cs009 <babidomacmillan@gmail.com>
2025-11-29 23:04:28 +0530

    Learned bubble sort program

---
+++ src/Day_06/Bubble_sort.java
@@ -0,0 +1,25 @@
+package Day_06;
+
+public class Bubble_sort {
+
+    public static void bubble_sort(int[] arr){
+        int n = arr.length;
+        for (int i = n - 1; i >= 1; i--){
+            for (int j = 0; j <= i - 1; j++){
+                if (arr[j] > arr[j + 1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {13,46,24,52,20,9};
+        bubble_sort(arr);
+        for (int i : arr){
+            System.out.print(i + " ");
+        }
+    }
+}
