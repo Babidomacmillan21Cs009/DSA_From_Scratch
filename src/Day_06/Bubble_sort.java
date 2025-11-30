88e4a01b685a1aea9a4e9ef70cf7b16d99ed9daf
Babidomacmillan21Cs009 <babidomacmillan@gmail.com>
2025-11-30 20:17:54 +0530

    Learned Recursive bubble sort

--- src/Day_06/Bubble_sort.java
+++ src/Day_06/Bubble_sort.java
@@ -15,11 +15,33 @@ public class Bubble_sort {
         }
     }
 
+    public static void buble_sort(int[] arr, int start, int end){
+        if (end == 0)    // only one element left . it already sorted
+            return;
+        // one full bubble sort
+        for (int i = start; i < end; i++){
+            if (arr[i] > arr[i + 1]){
+                int temp = arr[i + 1];
+                arr[i + 1] = arr[i];
+                arr[i] = temp;
+            }
+        }
+        // recursively sort remaining part
+        buble_sort(arr, start, end - 1);
+    }
+
     public static void main(String[] args) {
         int[] arr = {13,46,24,52,20,9};
         bubble_sort(arr);
         for (int i : arr){
             System.out.print(i + " ");
+        }
+        System.out.println();
+        System.out.println("--- Using Recursion ---");
+        int[] arr1 = {4,3,2,1};
+        buble_sort(arr1, 0, arr1.length - 1);
+        for (int i : arr1){
+            System.out.print(i+" ");
         }
     }
 }
