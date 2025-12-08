20080b3617726b05c6794ffaf9f9e1a97a8829e4
Babidomacmillan21Cs009 <babidomacmillan@gmail.com>
2025-12-08 12:13:38 +0530

    Learned optimal solution

--- src/Day_09/Rotate_array_by_K_places.java
+++ src/Day_09/Rotate_array_by_K_places.java
@@ -9,15 +9,33 @@ public class Rotate_array_by_K_places {
                 arr[i + 1] = arr[i];
             }
             arr[0] = temp;
-            rotateArray(arr,k - 1);
+            rotateArray(arr,k - 1);       // TC -> O(n * K) SC -> O(1)
         }
     }
 
+    public static void rotate(int[] arr, int k, int n){
+        k = k % n;
+        reverse(arr, 0, n - k - 1);
+        reverse(arr, n - k, n - 1);
+        reverse(arr, 0, n - 1);
+    }
+
+
+    public static void reverse(int[] arr, int start, int end){      // TC -> O(n)
+        while(start < end){
+           int temp = arr[start];
+           arr[start] = arr[end];
+           arr[end] = temp;
+           start++;
+           end--;
+        }
+    }
 
     public static void main(String[] args) {
         int[] arr = {1,2,3,4,5,6,7};
         int k = 3;
-        rotateArray(arr, k);
+//        rotateArray(arr, k);
+        rotate(arr, k, arr.length);
         for (int i : arr){
             System.out.print(i +" ");
         }
