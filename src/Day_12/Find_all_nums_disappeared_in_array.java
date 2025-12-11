83847d1c5c85643dae79e729c543734ddf30e61f
Babidomacmillan21Cs009 <babidomacmillan@gmail.com>
2025-12-11 17:00:03 +0530

    Done some programs

---
+++ src/Day_12/Find_all_nums_disappeared_in_array.java
@@ -0,0 +1,29 @@
+package Day_12;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Find_all_nums_disappeared_in_array {
+
+    public static List<Integer> findDisappearedNums(int[] arr, int n){
+        List<Integer> list = new ArrayList<>();
+
+        for (int i = 0; i < n; i++){
+            int index = (arr[i] - 1) % n;
+            arr[index] = arr[index] + n;
+        }
+
+        for (int i = 0; i < n; i++){
+            if (arr[i] <= n)
+                list.add(i + 1);
+        }
+
+        return list;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {4,3,2,7,8,2,3,1};
+        List<Integer> list = findDisappearedNums(arr, arr.length);
+        System.out.println(list);
+    }
+}
