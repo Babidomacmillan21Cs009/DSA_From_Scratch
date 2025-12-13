39c6b90bda30a0ef0410c1cb3f89c6d4ef1a9fce
Babidomacmillan21Cs009 <babidomacmillan@gmail.com>
2025-12-13 20:38:00 +0530

    Done Optimal soution

--- src/Day_14/SortZerosOnesAndTwos.java
+++ src/Day_14/SortZerosOnesAndTwos.java
@@ -29,9 +29,35 @@ public class SortZerosOnesAndTwos {
     // Time complexity - O(n)
     // Space complexity - O(1)
 
+
+    // Optimal  ---> Dutch National Flag Algorithm
+    public static void sortZerosOnesAndTwos(int[] arr, int n){
+        int low = 0, mid = 0, high = n -1;
+        while (mid <= high){
+            if (arr[mid] == 0){
+                int temp = arr[low];
+                arr[low] = arr[mid];
+                arr[mid] = temp;
+                low++;
+                mid++;
+            }
+            else if (arr[mid] == 1)
+                mid++;
+            else{
+                int temp = arr[high];
+                arr[high] = arr[mid];
+                arr[mid] = temp;
+                high--;
+            }
+        }
+        // Time complexity - O(n)
+        // Space complexity - O(1)
+    }
+
     public static void main(String[] args){
         int[] arr = {0,1,2,0,1,2,2,2,1,1,0,0,1};
-        sortZeroOneTwo(arr, arr.length);
+//        sortZeroOneTwo(arr, arr.length);
+        sortZerosOnesAndTwos(arr, arr.length);
         for (int i : arr){
             System.out.print(i+" ");
         }
