dfbd2bc5c4fed526d33b6475b727bddcf7f3ea4b
Babidomacmillan21Cs009 <babidomacmillan@gmail.com>
2025-12-15 13:27:13 +0530

    Done

--- src/Day_16/Rearrange_array_elements_by_sign.java
+++ src/Day_16/Rearrange_array_elements_by_sign.java
@@ -19,6 +19,24 @@ public class Rearrange_array_elements_by_sign {
             nums[2 * i + 1] = neg[i];
         }
     }
+    // Time complexity - O(2n)
+    // Space complexity - O(n)
+
+    public static int[] reArrageArray(int[] arr, int n){
+        int[] ans = new int[n];
+        int pIndex = 0, nIndex = 1;
+        for (int i = 0; i < n; i++){
+            if (arr[i] > 0){
+                ans[pIndex] = arr[i];
+                pIndex += 2;
+            }
+            else{
+                ans[nIndex] = arr[i];
+                nIndex += 2;
+            }
+        }
+        return ans;
+    }
     // Time complexity - O(n)
     // Space complexity - O(n)
 
@@ -26,8 +44,12 @@ public class Rearrange_array_elements_by_sign {
 
     public static void main(String[] args) {
         int[] nums = {3,1,-2,-5,2,-4};
-        reArrage(nums, nums.length);
-        for (int i : nums){
+//        reArrage(nums, nums.length);
+//        for (int i : nums){
+//            System.out.print(i+" ");
+//        }
+        int[] ans = reArrageArray(nums, nums.length);
+        for (int i : ans){
             System.out.print(i+" ");
         }
     }
