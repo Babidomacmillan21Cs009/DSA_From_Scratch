a933d56c7f23d834a4c4b1e434df5a9a7c33d6b9
Babidomacmillan21Cs009 <babidomacmillan@gmail.com>
2025-12-15 14:01:11 +0530

    Done optimal solution

--- src/Day_16/Rearrange_array_elements_by_sign.java
+++ src/Day_16/Rearrange_array_elements_by_sign.java
@@ -1,5 +1,8 @@
 package Day_16;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Rearrange_array_elements_by_sign {
 
     // Brute forch
@@ -22,6 +25,7 @@ public class Rearrange_array_elements_by_sign {
     // Time complexity - O(2n)
     // Space complexity - O(n)
 
+    // Optimal
     public static int[] reArrageArray(int[] arr, int n){
         int[] ans = new int[n];
         int pIndex = 0, nIndex = 1;
@@ -41,6 +45,37 @@ public class Rearrange_array_elements_by_sign {
     // Space complexity - O(n)
 
 
+    // If the array doesn't have equal num of Pos and Neg we do like this
+    public static void reArrangeArr(int[] nums, int n){
+        List<Integer> pos = new ArrayList<>();
+        List<Integer> neg = new ArrayList<>();
+        int posIndx = 0, negIndx = 1;
+        for (int i = 0; i < n; i++){
+            if (nums[i] > 0)
+                pos.add(nums[i]);
+            else
+                neg.add(nums[i]);
+        }
+        int l = 0, r = 0;
+        while (l < pos.size() && r < neg.size()){
+            nums[posIndx] = pos.get(l);
+            nums[negIndx] = neg.get(r);
+            posIndx += 2;
+            negIndx += 2;
+            l++;
+            r++;
+        }
+        int index = Math.min(pos.size(), neg.size()) * 2;
+        while (l < pos.size()){
+            nums[index++] = pos.get(l++);
+        }
+        while (r < neg.size()){
+            nums[index++] = neg.get(r++);
+        }
+        // Time complexity - O(n)
+        // Space complexity - O(n)
+    }
+
 
     public static void main(String[] args) {
         int[] nums = {3,1,-2,-5,2,-4};
@@ -48,8 +83,16 @@ public class Rearrange_array_elements_by_sign {
 //        for (int i : nums){
 //            System.out.print(i+" ");
 //        }
+        System.out.println(" if pos and neg are equal");
         int[] ans = reArrageArray(nums, nums.length);
         for (int i : ans){
+            System.out.print(i+" ");
+        }
+        System.out.println("\n");
+        System.out.println(" if pos != neg");
+        int[] arr = {1,2,4,-5,3,6};
+        reArrangeArr(arr, arr.length);
+        for (int i : arr){
             System.out.print(i+" ");
         }
     }
