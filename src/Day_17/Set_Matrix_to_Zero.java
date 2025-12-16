0ee8d807ac06c30261b08045f54476e39ad09f87
Babidomacmillan21Cs009 <babidomacmillan@gmail.com>
2025-12-16 21:38:30 +0530

    Learned optimal solution

--- src/Day_17/Set_Matrix_to_Zero.java
+++ src/Day_17/Set_Matrix_to_Zero.java
@@ -67,16 +67,66 @@ public class Set_Matrix_to_Zero {
     // Time complexity - O(2 * n * m)
     // Space complexity - O(n) + O(m)
 
+    // Optimal
+    public static void setMatrix(int[][] matrix){
+        int n = matrix.length;        // row --> matrix[..][0]
+        int m = matrix[0].length;     // col --> matrix[0][..]
+        int col0 = 1;
+
+        for (int i = 0; i < n; i++){
+            for (int j = 0; j < m; j++){
+                if (matrix[i][j] == 0){
+                    // mark i-th row
+                    matrix[i][0] = 0;
+
+                    // mark j-th row
+                    if (j != 0)
+                        matrix[0][j] = 0;
+                    else
+                        col0 = 0;
+                }
+            }
+        }
+
+        for (int i = 1; i < n; i++){
+            for (int j = 1; j < m; j++){
+                if (matrix[i][j] != 0){
+                    // check for col & row
+                    if (matrix[i][0] == 0 || matrix[0][j] == 0){
+                        matrix[i][j] = 0;
+                    }
+                }
+            }
+        }
+
+        // if first[0][0] guy is 0 then all first row will be 0
+        if (matrix[0][0] == 0) {
+            for (int j = 0; j < m; j++) {
+                matrix[0][j] = 0;
+            }
+        }
+
+        // if col0 is 0 then first col should be 0
+        if (col0 == 0){
+            for (int i = 0; i < n; i++){
+                matrix[i][0] = 0;
+            }
+        }
+    }
+    // Time complexity - O(2 * n * m)
+    // Space complexity - O(1)
+
     public static void main(String[] args) {
         int[][] matrix = {
                 {1,1,1,1},
-                {1,0,0,1},
+                {1,0,1,1},
                 {1,1,0,1},
-                {1,1,1,1}
+                {0,1,1,1}
         };
 
 //        setZero(matrix);
-        setMatrixZero(matrix);
+//        setMatrixZero(matrix);
+        setMatrix(matrix);
         for (int i = 0; i < matrix.length; i++){
             for (int j = 0; j < matrix[i].length; j++){
                 System.out.print(matrix[i][j]+" ");
