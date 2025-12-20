1738272d2ec8b60fde0b2a39f4cc4c1ff312d8b3
Babidomacmillan21Cs009 <babidomacmillan@gmail.com>
2025-12-20 21:26:57 +0530

    Done One program

--- src/Day_20/Majority_Element_II.java
+++ src/Day_20/Majority_Element_II.java
@@ -24,39 +24,56 @@ public class Majority_Element_II {
     public static List<Integer> majElem(int [] arr, int n){
         List<Integer> list = new ArrayList<>();
         Map<Integer, Integer> map = new HashMap<>();
-        for (int num : arr){
-            map.put(num, map.getOrDefault(num, 0)+ 1);
-        }
-
-        for (Map.Entry<Integer, Integer> entry : map.entrySet()){
-            if (entry.getValue() > n / 3)
-                list.add(entry.getKey());
+        for (int i = 0; i < n; i++){
+            map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
+            if (map.get(arr[i]) == (n / 3) + 1)  // add only once when count meets
+                list.add(arr[i]);
+            if (list.size() == 2) break;
         }
         return list;
     }
     // Time complexity - O(n)
     // Space complexity - O(n)
 
-    public static List<Integer> majElement(int [] arr, int n){
+    public static List<Integer> majorityElements(int[] arr, int n){
         List<Integer> list = new ArrayList<>();
-        Arrays.sort(arr);
-        int count = 1;
-        for (int i = 1; i < n; i++){
-            if (arr[i] == arr[i - 1])
-                count++;
-            if (count > n / 3)
-                list.add(arr[i]);
-            else
-                count = 1;
+        int count1 = 0, count2 = 0;
+        int can1 = 0, can2 = 0;
+        for (int i = 0; i < n; i++){
+            if (count1 == 0 && arr[i] != can2){         // Ensure two elements are unique
+                can1 = arr[i];
+                count1++;
+            } else if (count2 == 0 && arr[i] != can1){
+                can2 = arr[i];
+                count2++;
+            } else if (can1 == arr[i])
+                count1++;
+            else if (can2 == arr[i])
+                count2++;
+            else {
+                count1--;
+                count2--;
+            }
         }
+        // check the nums are appear more than n / 3 times
+        count1 = 0;    // it holds can1
+        count2 = 0;
+        for (int num : arr){
+            if (num == can1) count1++;
+            else if (num == can2) count2++;
+        }
+        if (count1 > n / 3) list.add(can1);
+        if (count2 > n / 3) list.add(can2);
         return list;
     }
+    // Time complexity - O(n);
+    // Space complexity - O(1);
 
 
     public static void main(String[] args) {
-        int[] arr = {1,2,3,1,2,1,1,2,2};
-        System.out.println(majorityElement(arr, arr.length));
-        System.out.println(majElem(arr, arr.length));
-        System.out.println(majElement(arr, arr.length));
+        int[] arr = {3,2,3};
+//        System.out.println(majorityElement(arr, arr.length));
+//        System.out.println(majElem(arr, arr.length));  // HashMap
+        System.out.println(majorityElements(arr, arr.length));
     }
 }
