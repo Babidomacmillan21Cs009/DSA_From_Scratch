be8f4230685776ac1e2c69d8494eec376cd832db
Babidomacmillan21Cs009 <babidomacmillan@gmail.com>
2025-12-21 20:52:48 +0530

    Learned better solution

--- src/Day_21/Three_Sum.java
+++ src/Day_21/Three_Sum.java
@@ -26,9 +26,30 @@ public class Three_Sum {
     // Time complexity - O(n^3)
     // Space complexity - O(k)
 
+    public static List<List<Integer>> threeSumBetter(int[] arr, int n){
+        Set<List<Integer>> set = new HashSet<>();
+        for (int i = 0; i < n; i++){
+            Set<Integer> hashSet = new HashSet<>();
+            for (int j = i + 1; j < n; j++){
+                int third = -(arr[i] + arr[j]);
+                if (hashSet.contains(third)){
+                    List<Integer> temp = new ArrayList<>();
+                    temp.add(arr[i]);
+                    temp.add(arr[j]);
+                    temp.add(third);
+                    Collections.sort(temp);
+                    set.add(temp);
+                }
+                hashSet.add(arr[j]);
+            }
+        }
+        return new ArrayList<>(set);
+    }
+
 
     public static void main(String[] args) {
         int[] arr = {-1,0,1,2,-1,-4};
         System.out.println(threeSum(arr, arr.length));
+        System.out.println(threeSumBetter(arr, arr.length));
     }
 }
